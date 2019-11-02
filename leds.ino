faaabf7238298a47b2c85affbbe94f8821fdb6e9
kathy-jiang <55174470+kathy-jiang@users.noreply.github.com>
2019-11-01 21:10:58 -0700

    Create leds.ino

---
+++ leds.ino
@@ -0,0 +1,20 @@
+int led = 12;
+int led2 = 12;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led, OUTPUT);
+  pinMode(led2, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(led, HIGH);
+  digitalWrite(led2,HIGH);
+  delay(100);
+  digitalWrite(led, LOW);
+  digitalWrite(led2,LOW);
+  delay(100);
+
+}
